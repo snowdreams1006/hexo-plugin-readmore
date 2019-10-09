6d49e38a4f8c4efd7559f4942b0d5e2d5c38876a
snowdreams1006 <snowdreams1006@163.com>
2019-10-09 22:46:35 +0800

    after_post_render

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const hexo = hexo || {};
-const config = hexo.config;
-// const addlink = hexo.config.addlink;
-
-// hexo.extend.filter.register('before_post_render', data => {
-//   if (!addlink || !config.url || data.layout !== 'post') {
-//     return data;
-//   }
-
-//   let domain = config.url;
-//   let beforeText = addlink.before_text || '';
-//   let afterText = addlink.after_text || '';
-//   let layout = data.layout;
-//   let href = `${domain}/${data.path}`;
-//   let hrefText = `${beforeText} [${href}](${href}) ${afterText}`;
-
-//   data.content += `\n\n${hrefText}`;
-// });
-
 hexo.extend.filter.register('before_post_render', function(data){
+  console.log('before_post_render');
+
   data.title = data.title.toLowerCase();
 
   console.log(`before_post_render=${data}`);
@@ -26,6 +9,8 @@ hexo.extend.filter.register('before_post_render', function(data){
 });
 
 hexo.extend.filter.register('after_post_render', function(data){
+  console.log('after_post_render');
+  
   data.content = data.content.replace(/@(\d+)/, '<a href="http://twitter.com/$1">#$1</a>');
 
   console.log(`after_post_render=${data}`);

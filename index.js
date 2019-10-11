cb98b80404b1df4e01bb788f9246fef37aca653b
snowdreams1006 <snowdreams1006@163.com>
2019-10-11 10:25:37 +0800

    modify licese

--- index.js
+++ index.js
@@ -1,19 +1,3 @@
 hexo.extend.filter.register('before_post_render', function(data) {
 	console.log('before_post_render:' + data);
-
-	// data.title = data.title.toLowerCase();
-
-	// console.log(`before_post_render=${data}`);
-
-	// return data;
-});
-
-hexo.extend.filter.register('after_post_render', function(data) {
-	console.log('after_post_render:' + data);
-
-	// data.content = data.content.replace(/@(\d+)/, '<a href="http://twitter.com/$1">#$1</a>');
-
-	// console.log(`after_post_render=${data}`);
-
-	// return data;
 });
